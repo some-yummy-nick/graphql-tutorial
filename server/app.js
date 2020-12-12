2bf92a19f347f70d0152216e6733a07eb001e6d4
Marat <shaimardanov@infomatika.ru>
2020-12-12 21:08:45 +0300

    Update mongoDB config

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema.js');
 const mongoose = require('mongoose');
-const cors= require("cors");
+const cors = require("cors");
 
-const {MONGODB} = require("./config");
-
+const MONGODB_LOGIN = process.env.MONGODB_LOGIN;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+const url = `mongodb+srv://${MONGODB_LOGIN}:${MONGODB_PASSWORD}@cluster0.qgfv4.mongodb.net/graphql-tutorial?retryWrites=true&w=majority`;
 const app = express();
 
 app.use(cors());
@@ -15,7 +17,7 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false })
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => app.listen({port: 5005}));
 
 
